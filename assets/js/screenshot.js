5721fd8a419e8a6caa45e7f35c6ea16e7025fb41
Fabio de Melo <fabiomessias98@outlook.com>
2022-06-03 02:21:47 -0300

    Implementacao do menu

--- assets/js/screenshot.js
+++ assets/js/screenshot.js
@@ -1,20 +1,52 @@
-// setTimeout(function() {
-//     function capture() {
-//         const captureElement = document.querySelector('body')
-//         html2canvas(captureElement)
-//             .then(canvas => {
-//                 canvas.style.display = 'none'
-//                 document.body.appendChild(canvas)
-//                 return canvas
-//             })
-//             .then(canvas => {
-//                 const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
-//                 const a = document.createElement('a')
-//                 a.setAttribute('download', 'my-image.png')
-//                 a.setAttribute('href', image)
-//                 a.click()
-//                 canvas.remove()
-//             })
-//     }
-//     capture()
-// }, 3000)
+(function() {
+    const setting = document.querySelector( '.js-setting' )
+    const widget = document.querySelector( '.js-widget' )
+    const close = document.querySelector( '.js-close' )
+    const screenshort = document.querySelector( '.js-screenshot' )
+    
+
+    setting.addEventListener( 'click', function() {
+        widget.classList.add( 'is-active' )
+    })
+
+    close.addEventListener( 'click', function() {
+        widget.classList.remove( 'is-active' )
+    })
+
+    screenshort.addEventListener( 'click', function() {
+        widget.classList.remove( 'is-active' )
+
+        setTimeout(function() {
+            capture()
+        }, 1000)
+
+        hiddenSetting()
+    })
+
+    function hiddenSetting() {
+        setting.classList.add( 'is-disabled' )
+
+        setTimeout(function() {
+            setting.classList.remove( 'is-disabled' )
+        }, 3000)
+    }
+
+    function capture() {
+        const captureElement = document.querySelector('body')
+
+        html2canvas(captureElement)
+            .then(canvas => {
+                canvas.style.display = 'none'
+                document.body.appendChild(canvas)
+                return canvas
+            })
+            .then(canvas => {
+                const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+                const a = document.createElement('a')
+                a.setAttribute('download', 'my-image.png')
+                a.setAttribute('href', image)
+                a.click()
+                canvas.remove()
+            })
+    }
+})()
